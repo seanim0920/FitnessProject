f3ec69cc430a13ce9ec126ed28072a842f5bc3e4
mhayes853 <matthewha853@gmail.com>
2025-01-29 15:47:26 -0800

    Sun Movement

--- journaling/SunBackground.tsx
+++ journaling/SunBackground.tsx
@@ -16,10 +16,62 @@ import {
   withTiming
 } from "react-native-reanimated"
 import { EdgeInsets } from "react-native-safe-area-context"
-import { MovingCloudsDrawing, cloud } from "./Clouds"
+import { Cloud, MovingCloudsDrawing, cloud } from "./Clouds"
+
+export type SunBackgroundColor = string
+
+export type SunGradient = [
+  SunBackgroundColor,
+  SunBackgroundColor,
+  SunBackgroundColor,
+  SunBackgroundColor
+]
+
+export type SkyGradient = SunBackgroundColor[]
+
+export type SunBackground = {
+  sun: {
+    relativePosition: { x: number; y: number }
+    gradient: SunGradient
+  }
+  skyGradient: SkyGradient
+  clouds: Cloud[]
+}
+
+export const sunRelativePosition = (x: number) => {
+  return { x, y: Math.sin(3.125 * x) * 0.175 }
+}
+
+export const MID_DAY_SUN_GRADIENT: SunGradient = [
+  "#FFFCF0",
+  "#FCF0BE",
+  "#FAEAA5",
+  "#F9E48C"
+]
+
+export const MORNING_DAY_SUN_GRADIENT: SunGradient = [
+  "#FEDA6F",
+  "#FBBC6B",
+  "#F9AE6A",
+  "#F89F68"
+]
+
+export const MID_DAY_SKY_GRADIENT: SkyGradient = [
+  "#3FADFB",
+  "#6CACFF",
+  "#3183FD"
+]
+
+export const SUNRISE_SKY_GRADIENT: SkyGradient = [
+  "#FF7040",
+  "#F57A86",
+  "#65C5F6",
+  "#9ADDFA"
+]
 
 export type SunBackgroundProps = {
   size: SkSize
+  background: SunBackground
   edgeInsets: EdgeInsets
 }
 
@@ -28,50 +80,45 @@ const OUTER_RING_RADIUS = 56
 const FADE_RING_RADIUS = 64
 const FADE_RING_TARGET_RADIUS = 80
 
-const CLOUDS_1 = [
-  cloud({
-    relativeX: 0.6,
-    relativeY: 0.1,
-    relativeRangeX: 0.1,
-    scale: 0.3,
-    speed: 30_000
-  }),
-  cloud({
-    relativeX: 0.4,
-    relativeY: 0.15,
-    relativeRangeX: 0.15,
-    scale: 0.5,
-    speed: 23_000
-  }),
-  cloud({
-    relativeX: 0.6,
-    relativeY: 0.17,
-    relativeRangeX: 0.15,
-    scale: 0.55,
-    speed: 21_000
-  }),
-  cloud({
-    relativeX: 0.03,
-    relativeY: 0.23,
-    relativeRangeX: 0.2,
-    scale: 0.7,
-    speed: 16_000
-  }),
-  cloud({
-    relativeX: 0.2,
-    relativeY: 0.26,
-    relativeRangeX: 0.25,
-    scale: 0.8,
-    speed: 12_000
-  })
-]
-
 export const SunBackgroundDrawing = ({
   edgeInsets,
+  background,
   size
 }: SunBackgroundProps) => {
-  const sunX = FADE_RING_RADIUS + 24
-  const sunY = edgeInsets.top + FADE_RING_RADIUS
+  return (
+    <Group>
+      <Rect width={size.width} height={size.height}>
+        <LinearGradient
+          // start={vec(-size.width * 1.1, size.height / 4)}
+          start={vec(size.width / 2, 0)}
+          // end={vec(size.width, size.height)}
+          end={vec(size.width / 2, size.height)}
+          colors={background.skyGradient}
+          positions={[0.3, 0.45, 0.8, 1]}
+        />
+      </Rect>
+      <SunDrawing sun={background.sun} size={size} edgeInsets={edgeInsets} />
+      <MovingCloudsDrawing size={size} clouds={background.clouds} />
+    </Group>
+  )
+}
+
+export type SunProps = {
+  sun: SunBackground["sun"]
+  size: SkSize
+  edgeInsets: EdgeInsets
+}
+
+export const SunDrawing = ({ sun, size, edgeInsets }: SunProps) => {
+  const insetSize = {
+    width: size.width - edgeInsets.left - edgeInsets.right,
+    height: size.height - edgeInsets.top - edgeInsets.bottom
+  }
+  const insetHeightDiff = size.height - insetSize.height
+  const relativeY =
+    sun.relativePosition.y * (insetSize.width / insetSize.height)
+  const sunX = size.width * sun.relativePosition.x
+  const sunY = FADE_RING_RADIUS + insetHeightDiff - insetSize.height * relativeY
   const ringRadius = useSharedValue(FADE_RING_RADIUS)
   const ringOpacity = useSharedValue(0)
   useEffect(() => {
@@ -100,18 +147,11 @@ export const SunBackgroundDrawing = ({
   }, [ringRadius, ringOpacity])
   return (
     <Group>
-      <Rect width={size.width} height={size.height}>
-        <LinearGradient
-          start={vec(0, 0)}
-          end={vec(size.width, size.height)}
-          colors={["#3AC4F7", "#6AA5F2", "#9589F7"]}
-        />
-      </Rect>
       <Circle cx={sunX} cy={sunY} r={CORE_RADIUS}>
         <RadialGradient
           c={{ x: sunX, y: sunY }}
           r={CORE_RADIUS}
-          colors={["#FFFCF0", "#FCF0BE", "#FAEAA5", "#F9E48C"]}
+          colors={sun.gradient}
           positions={[0.5, 0.75, 0.9, 1]}
         />
       </Circle>
@@ -119,7 +159,7 @@ export const SunBackgroundDrawing = ({
         cx={sunX}
         cy={sunY}
         r={OUTER_RING_RADIUS}
-        color="#FBE277"
+        color={sun.gradient[3]}
         style="stroke"
         strokeWidth={4}
       />
@@ -127,12 +167,11 @@ export const SunBackgroundDrawing = ({
         cx={sunX}
         cy={sunY}
         r={ringRadius}
-        color="#FBE277"
+        color={sun.gradient[3]}
         style="stroke"
         opacity={ringOpacity}
         strokeWidth={4}
       />
-      <MovingCloudsDrawing size={size} clouds={CLOUDS_1} />
     </Group>
   )
 }
